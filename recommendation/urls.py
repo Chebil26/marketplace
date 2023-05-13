73d7e9c68ba22b12dd4a869e6ac83955b384cc0a
Chebil <aminechebil33@gmail.com>
2023-05-13 05:25:18 +0100

    ff

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import book_recommendation
+# from .views import book_recommendation
 
 urlpatterns = [
-    path('book_recommendation/<str:book_name>/', book_recommendation, name='book_recommendation'),
+    # path('book_recommendation/<str:book_name>/', book_recommendation, name='book_recommendation'),
 ]
