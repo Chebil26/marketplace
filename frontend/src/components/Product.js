9256b27655ed7012eb02137a627f4d21fa24f28f
Chebil <aminechebil33@gmail.com>
2023-05-13 23:13:23 +0100

    image test 1

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -13,7 +13,7 @@ function Product({ product }) {
         <Card.Img
           src={
             product.image
-              ? `${process.env.REACT_APP_API_SERVER}${product.image}`
+              ? `https://adeem-2se9.onrender.com/images/The_Sound_Of_Waves_5rpr58Y.jpg`
               : product.defaultImage
               ? product.defaultImage
               : placeholder
